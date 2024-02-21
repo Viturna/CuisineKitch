63c19ed1c96553794d5e4a6d75fa34a6c3babe40
LineCab <line.cabaret@mmibordeaux.com>
2024-02-21 20:14:59 +0100

    css formulaire

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,5 +2,11 @@ module RecipesHelper
 end
 
 def total_time(recipe)
-  (recipe.preparationtime.min + recipe.cookingtime.min + recipe.restingtime.min)
+  preparation_time = recipe.preparationtime ? recipe.preparationtime.min : 0
+  cooking_time = recipe.cookingtime ? recipe.cookingtime.min : 0
+  resting_time = recipe.restingtime ? recipe.restingtime.min : 0
+
+  preparation_time + cooking_time + resting_time
 end
+
+
