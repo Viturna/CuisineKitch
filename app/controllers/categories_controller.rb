000e2710d5f169ed4ca03d30a3f402a6b33ace1c
LineCab <line.cabaret@mmibordeaux.com>
2024-02-22 15:51:06 +0100

    je sais plus

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -56,7 +56,6 @@ class CategoriesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
