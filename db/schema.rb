9f26026296e2af59e0d005ebbb23c1e5fc482b0a
LineCab <line.cabaret@mmibordeaux.com>
2024-02-22 19:40:55 +0100

    on oublie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_182008) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_184006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,15 +70,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_182008) do
     t.text "image"
     t.string "price"
     t.string "difficulty"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "statut"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email"
   end
 
   add_foreign_key "recipe_categories", "categories"
