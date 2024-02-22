183780e25e983dc01fd3b9f2498cac0066ed17af
Viturna <viturnapro@gmail.com>
2024-02-22 19:20:58 +0100

    user delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_173847) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_182008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_173847) do
     t.string "statut"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   add_foreign_key "recipe_categories", "categories"
