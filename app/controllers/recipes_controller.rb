2b1027ac48f610c38f700d542eb9f9e53a204623
Viturna <viturnapro@gmail.com>
2024-02-24 21:41:13 +0100

    Ingrédient qui marche enfin et number pour l'input nb person

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,25 +1,15 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[show edit update destroy]
+  before_action :set_recipe, only: %i[ show edit update destroy ]
 
   # GET /recipes or /recipes.json
   def index
-    @categories = Category.all
-    @recipes = Recipe.all
-  end
-
-  # GET /recipes/apropos
-  def apropos
+     @categories = Category.all
     @recipes = Recipe.all
   end
 
   # GET /recipes/1 or /recipes/1.json
   def show
-    if params[:id] == "apropos"
-      render 'apropos'
-    else
-      @recipe = Recipe.find(params[:id])
-      @ingredients = @recipe.ingredients
-    end
+    @recipe = Recipe.find(params[:id])
   end
 
   # GET /recipes/new
@@ -29,36 +19,47 @@ class RecipesController < ApplicationController
     @unities = {}
   end
 
-  # GET /recipes/1/edit
-  def edit
-    @recipe.recipe_ingredients.each do |recipe_ingredient|
-      @quantities[recipe_ingredient.ingredient_id] = recipe_ingredient.quantity
-      @unities[recipe_ingredient.ingredient_id] = recipe_ingredient.unity
-    end
+
+# GET /recipes/1/edit
+def edit
+  @recipe = Recipe.find(params[:id])
+  @quantities = {}
+  @unities = {}
+
+  # Pour chaque ingrédient de la recette, récupérez la quantité et l'unité
+  @recipe.recipe_ingredients.each do |recipe_ingredient|
+    @quantities[recipe_ingredient.ingredient_id] = recipe_ingredient.quantity
+    @unities[recipe_ingredient.ingredient_id] = recipe_ingredient.unity
   end
+end
+
 
   # POST /recipes or /recipes.json
   def create
-    @recipe = Recipe.new(recipe_params)
-  
-    respond_to do |format|
-      if @recipe.save
-        save_recipe_ingredients(@recipe)  # Appel de la méthode pour sauvegarder les ingrédients
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
-        format.json { render :show, status: :created, location: @recipe }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
+    @recipe = Recipe.new(recipe_params.except(:ingredient_quantities, :ingredient_unities))
+
+    if @recipe.save
+      # Enregistrez les ingrédients avec leurs quantités et unités
+      save_ingredients(recipe_params[:ingredient_quantities], recipe_params[:ingredient_unities])
+
+      redirect_to @recipe, notice: 'Recipe was successfully created.'
+    else
+      render :new
     end
   end
-  
+
   # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
+    @recipe = Recipe.find(params[:id])
+
     respond_to do |format|
       if @recipe.update(recipe_params)
+        # Supprime d'abord tous les ingrédients de la recette
         @recipe.recipe_ingredients.destroy_all
+
+        # Enregistre ensuite les nouveaux ingrédients avec les nouvelles valeurs
         save_recipe_ingredients(@recipe)
+
         format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
         format.json { render :show, status: :ok, location: @recipe }
       else
@@ -68,10 +69,12 @@ class RecipesController < ApplicationController
     end
   end
 
+
   # DELETE /recipes/1 or /recipes/1.json
   def destroy
     delete_associated_entries(@recipe)
     @recipe.destroy!
+
     respond_to do |format|
       format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
       format.json { head :no_content }
@@ -79,47 +82,34 @@ class RecipesController < ApplicationController
   end
 
   private
-    # Utilisez des callbacks pour partager la configuration commune ou les contraintes entre actions.
+    # Use callbacks to share common setup or constraints between actions.
     def set_recipe
-      if params[:id] != "apropos"
-        @recipe = Recipe.find(params[:id])
-      end
+      @recipe = Recipe.find(params[:id])
     end
 
-    # Seuls les paramètres autorisés sont autorisés.
     def recipe_params
-      params.require(:recipe).permit(:title, :preparationtime, :cookingtime, :restingtime, :description, :price, :difficulty, :step1, :step2, :step3, :step4, :step5, :step6, :step7, :step8, :step9, :step10, :image, :nbperson, category_ids: [], ingredient_ids: [], ingredient_quantities: [], ingredient_unitys: [])
+      params.require(:recipe).permit(
+        :title, :preparationtime, :cookingtime, :restingtime, :description,
+        :difficulty, :price, :nbperson, :step1, :step2, :step3, :step4, :step5,
+        :step6, :step7, :step8, :step9, :step10, :image,
+        category_ids: [],
+        ingredient_quantities: {},
+        ingredient_unities: {}
+      )
     end
 
-    # Méthode pour sauvegarder les ingrédients d'une recette
-    def save_recipe_ingredients(recipe)
-      ingredient_ids = params.dig(:recipe, :ingredient_ids)
-      ingredient_quantities = params.dig(:recipe, :ingredient_quantities)
-      ingredient_unities = params.dig(:recipe, :ingredient_unities)  # Correction du nom du paramètre
-    
-      return if ingredient_ids.nil? || ingredient_quantities.nil? || ingredient_unities.nil?
-    
-      ingredient_ids.each do |ingredient_id|
-        quantity = ingredient_quantities[ingredient_id.to_sym]
-        unity = ingredient_unities[ingredient_id.to_sym]  # Correction du nom de la variable
-    
-        next if quantity.blank? || unity.blank?
-    
+    def save_ingredients(quantities, unities)
+      quantities.each do |ingredient_id, quantity|
+        unity = unities[ingredient_id]
         ingredient = Ingredient.find(ingredient_id)
-    
-        recipe.recipe_ingredients.create(
-          ingredient_id: ingredient_id,
-          quantity: quantity,
-          unity: unity,
-          title: ingredient.title,
-          image: ingredient.image
-        )
+        # Créez une instance RecipeIngredient associant l'ingrédient à la recette avec la quantité et l'unité
+        @recipe.recipe_ingredients.create(ingredient_id: ingredient_id, title: ingredient.title, image: ingredient.image, quantity: quantity, unity: unity)
       end
     end
-    
-    # Méthode pour supprimer les entrées associées à une recette
+
     def delete_associated_entries(recipe)
       recipe.recipe_ingredients.destroy_all
       recipe.recipe_categories.destroy_all
     end
+
 end
